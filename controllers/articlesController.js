e7129e7b2619a42bbb896eba540c740d42d3841b
mecaputy <martina.e.caputy@gmail.com>
2018-07-21 16:06:30 -0400

    edit

---
+++ controllers/articlesController.js
@@ -0,0 +1,9 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+        dn.Article
+        .find(req.query)
+        //  look at AJAX BOOKS for similar code 
+    }
+}
